e5e6def03f1087fd531d487c71fd0a51775bb3ac
2KAY-kat <dambwedesigns@gmail.com>
2026-02-26 07:51:10 +0200

    scafolding the folder structure the sites'scomponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+import Hero from "./sections/Hero"
+import Services from "./sections/Services"
+import Projects from "./sections/Projects"
+import Skills from "./sections/Skills"
+import Contact from "./sections/Contact"
 
 function App() {
-
   return (
-    <div>
-      Zennertekh
+    <div className="bg-slate-950 text-white">
+      <Navbar />
+      <Hero />
+      <Services />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   )
 }
